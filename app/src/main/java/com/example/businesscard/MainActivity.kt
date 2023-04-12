23d9ebd0656e15fd83eada96bf0566cde005a360
Vlad <tsoiivv@gmail.com>
2023-04-13 01:41:14 +0600

    Finish the project

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.businesscard
 
-import android.R
-import android.app.ActionBar
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +15,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.textHello
-        //supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.white)))
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.white)))
 
         binding.button.setOnClickListener {
             Intent(this, SecondActivity::class.java).also {
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
                 startActivity(it)
             }
         }
